0b4c76ab2efc95b193baa6bd16004fe658f915dc
KarenPe <94763878+KarenPe@users.noreply.github.com>
2021-12-31 17:04:37 +0200

    Create dataform.h

---
+++ dataform.h
@@ -0,0 +1,25 @@
+#ifndef DATAFORM_H
+#define DATAFORM_H
+
+#include <QWidget>
+
+namespace Ui {
+class DataForm;
+}
+
+class DataForm : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit DataForm(QWidget *parent = nullptr);
+
+    void pushData(const QString& text);
+
+    ~DataForm();
+
+private:
+    Ui::DataForm *ui;
+};
+
+#endif // DATAFORM_H
